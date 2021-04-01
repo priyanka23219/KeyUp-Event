5e931021dd7313a3ca099d5f9a597e28f649f2e5
Priyanka <priyanka.23219@gmail.com>
2021-04-01 18:05:44 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,45 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'KeyUpEvent';
+  
+  // getValue1(i: { target: { value: any; }; })
+  // {
+  //   console.warn(i.target.value)
+  // }
+  getValue1(i: any)
+  {
+    console.warn(i)
+  }
+  getValue2(i: any)
+  {
+    console.warn(i)
+  }
+  getValue3(i: any)
+  {
+    console.warn(i)
+  }
+  getValue4(i: any)
+  {
+    console.warn(i)
+  }
+  getValue5(i: any)
+  {
+    console.warn(i)
+  }
+  getValue6(i: any)
+  {
+    console.warn(i)
+  }
+  getValue7(i: any)
+  {
+    console.warn(i)
+  }
+  getValue8(i: any)
+  {
+    console.warn(i)
+  }
+
 }
